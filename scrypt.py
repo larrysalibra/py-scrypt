89a093a138250e2b0005d4cef60d0f836ac551c2
mhallin <mhallin@gmail.com>
2012-10-18 16:40:06 +0200

    Typos in setup script

--- scrypt.py
+++ scrypt.py
@@ -11,40 +11,40 @@ from ctypes import (cdll,
 _scrypt = cdll.LoadLibrary(imp.find_module('_scrypt')[1])
 
 _scryptenc_buf = _scrypt.scryptenc_buf
-_scryptenc_buf.argtypes = [c_char_p, # const uint_t  *inbuf
-                           c_size_t, # size_t         inbuflen
-                           c_char_p, # uint8_t       *outbuf
-                           c_char_p, # const uint8_t *passwd
-                           c_size_t, # size_t         passwdlen
-                           c_size_t, # size_t         maxmem
-                           c_double, # double         maxmemfrac
-                           c_double, # double         maxtime
+_scryptenc_buf.argtypes = [c_char_p,  # const uint_t  *inbuf
+                           c_size_t,  # size_t         inbuflen
+                           c_char_p,  # uint8_t       *outbuf
+                           c_char_p,  # const uint8_t *passwd
+                           c_size_t,  # size_t         passwdlen
+                           c_size_t,  # size_t         maxmem
+                           c_double,  # double         maxmemfrac
+                           c_double,  # double         maxtime
                            ]
 _scryptenc_buf.restype = c_int
 
 _scryptdec_buf = _scrypt.scryptdec_buf
-_scryptdec_buf.argtypes = [c_char_p,          # const uint8_t *inbuf
-                           c_size_t,          # size_t         inbuflen
-                           c_char_p,          # uint8_t       *outbuf
-                           POINTER(c_size_t), # size_t        *outlen
-                           c_char_p,          # const uint8_t *passwd
-                           c_size_t,          # size_t         passwdlen
-                           c_size_t,          # size_t         maxmem
-                           c_double,          # double         maxmemfrac
-                           c_double,          # double         maxtime
+_scryptdec_buf.argtypes = [c_char_p,           # const uint8_t *inbuf
+                           c_size_t,           # size_t         inbuflen
+                           c_char_p,           # uint8_t       *outbuf
+                           POINTER(c_size_t),  # size_t        *outlen
+                           c_char_p,           # const uint8_t *passwd
+                           c_size_t,           # size_t         passwdlen
+                           c_size_t,           # size_t         maxmem
+                           c_double,           # double         maxmemfrac
+                           c_double,           # double         maxtime
                            ]
 _scryptdec_buf.restype = c_int
 
 _crypto_scrypt = _scrypt.crypto_scrypt
-_crypto_scrypt.argtypes = [c_char_p, # const uint8_t *passwd
-                           c_size_t, # size_t         passwdlen
-                           c_char_p, # const uint8_t *salt
-                           c_size_t, # size_t         saltlen
-                           c_uint64, # uint64_t       N
-                           c_uint32, # uint32_t       r
-                           c_uint32, # uint32_t       p
-                           c_char_p, # uint8_t       *buf
-                           c_size_t, # size_t         buflen
+_crypto_scrypt.argtypes = [c_char_p,  # const uint8_t *passwd
+                           c_size_t,  # size_t         passwdlen
+                           c_char_p,  # const uint8_t *salt
+                           c_size_t,  # size_t         saltlen
+                           c_uint64,  # uint64_t       N
+                           c_uint32,  # uint32_t       r
+                           c_uint32,  # uint32_t       p
+                           c_char_p,  # uint8_t       *buf
+                           c_size_t,  # size_t         buflen
                            ]
 _crypto_scrypt.restype = c_int
 
@@ -70,6 +70,7 @@ MAXTIME_DEFAULT_ENC = 5.0
 
 IS_PY2 = sys.version_info < (3, 0, 0, 'final', 0)
 
+
 class error(Exception):
     def __init__(self, scrypt_code):
         if isinstance(scrypt_code, int):
@@ -84,7 +85,9 @@ def encrypt(input, password,
             maxtime=MAXTIME_DEFAULT_ENC,
             maxmem=MAXMEM_DEFAULT,
             maxmemfrac=MAXMEMFRAC_DEFAULT):
-    """encrypt(input, password, maxtime=5.0, maxmem=0, maxmemfrac=0.125): str; encrypt a string"""
+    """encrypt(input, password, maxtime=5.0, maxmem=0, maxmemfrac=0.125): str
+
+    encrypt a string"""
 
     if IS_PY2 and isinstance(input, unicode):
         raise TypeError('input must be type str')
@@ -106,12 +109,15 @@ def encrypt(input, password,
 
     return outbuf.raw
 
+
 def decrypt(input, password,
             maxtime=MAXTIME_DEFAULT,
             maxmem=MAXMEM_DEFAULT,
             maxmemfrac=MAXMEMFRAC_DEFAULT,
             encoding='utf-8'):
-    """decrypt(input, password, maxtime=300.0, maxmem=0, maxmemfrac=0.5): str; decrypt a string"""
+    """decrypt(input, password, maxtime=300.0, maxmem=0, maxmemfrac=0.5): str
+
+    decrypt a string"""
 
     outbuf = create_string_buffer(len(input))
     outbuflen = pointer(c_size_t(0))
@@ -134,8 +140,11 @@ def decrypt(input, password,
 
     return str(out_bytes, encoding)
 
-def hash(password, salt, N=1<<14, r=8, p=1):
-    """hash(password, salt, N=2**14, r=8, p=1): str; compute a 64-byte scrypt hash"""
+
+def hash(password, salt, N=1 << 14, r=8, p=1):
+    """hash(password, salt, N=2**14, r=8, p=1): str
+
+    compute a 64-byte scrypt hash"""
 
     outbuf = create_string_buffer(64)
 
@@ -144,7 +153,7 @@ def hash(password, salt, N=1<<14, r=8, p=1):
     if not IS_PY2 and isinstance(salt, str):
         salt = bytes(salt, 'utf-8')
 
-    if r * p >= (1 << 30) or N <= 1 or (N & (N-1)) != 0 or p < 1 or r < 1:
+    if r * p >= (1 << 30) or N <= 1 or (N & (N - 1)) != 0 or p < 1 or r < 1:
         raise error('hash parameters are wrong (r*p should be < 2**30, and N should be a power of two > 1)')
 
     result = _crypto_scrypt(password, len(password),
@@ -156,5 +165,6 @@ def hash(password, salt, N=1<<14, r=8, p=1):
         raise error('could not compute hash')
 
     return outbuf.raw
+
 
 __all__ = ['error', 'encrypt', 'decrypt', 'hash']
